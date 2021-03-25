4d3eb0641f613d5267e834f2142f53edd8f5fd0b
rogier <rogierkrijnen@live.nl>
2021-03-25 13:39:06 +0100

    In bash launching via roslaunch is waiting, thus to launch two nodes port all the other launch files that need to be called to one specific launch file and call taht one

--- src/hardware_interface/senseglove_hardware_interface/launch/startGlove.sh
+++ src/hardware_interface/senseglove_hardware_interface/launch/startGlove.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Start SenseCom
-$1/../SenseGlove_API/SenseCom/SenseCom.x86_64 &
+$1/SenseCom/Linux/SenseCom.x86_64 &
 
 # Allow SenseComm to start
-sleep 3
+sleep 15
 
 # Start SenseGlove node
 source ~/.bashrc
-roslaunch senseglove_hardware_interface hardware.launch robot:=dk1 nr_of_glove:=1 is_right:=false
-roslaunch senseglove_hardware_interface hardware.launch robot:=dk1 nr_of_glove:=2 is_right:=true
+roslaunch senseglove_launch senseglove_hardware_demo.launch
 
 # Kill SenseComm when node is killed
 killall -9 SenseCom.x86_64
