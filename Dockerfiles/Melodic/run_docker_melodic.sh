03e87a162756b61454ce8ce8ebd844756ae16c7c
deakensbubbles <rogierkrijnen@live.nl>
2022-07-18 13:47:34 +0200

    Add Dockerfiles for Melodic and Netic distros and corresponding startup scripts

---
+++ Dockerfiles/Melodic/run_docker_melodic.sh
@@ -0,0 +1,10 @@
+# Original command (broke)
+sudo docker run -it --privileged --net=host --gpus all \
+    --device=/dev/ttyACM0 \
+    --device=/dev/ttyACM1 \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    dockerfile_nvidia_ros_melodic
+ 
