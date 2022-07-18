e49944f4427350777ae24895c2a4aae16b2a307a
rogier krijnen <rogierkrijnen@live.nl>
2022-07-18 17:01:29 +0200

    Add Dockerfiles for windows hosts

--- Dockerfiles/Melodic/run_docker_melodic.sh
+++ Dockerfiles/Melodic/run_docker_melodic.sh
@@ -1,7 +1,5 @@
-# Original command (broke)
-sudo docker run -it --privileged --net=host --gpus all \
-    --device=/dev/ttyACM0 \
-    --device=/dev/ttyACM1 \
+docker run -it --privileged --net=host --gpus all \
+    --device="class/{4d36e978-e325-11ce-bfc1-08002be10318}" \
     --env="NVIDIA_DRIVER_CAPABILITIES=all" \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
