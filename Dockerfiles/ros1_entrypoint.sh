02a8d7475136e6ce69b3cc89306c1c129b40686d
Akshay Radhamohan Menon <akshay@senseglove.com>
2025-06-04 23:50:13 +0200

    Docker file with GPG key fixes & CMake upgrade, along with entrypoint

---
+++ Dockerfiles/ros1_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+source /opt/ros/noetic/setup.bash
+
+if [ -f /root/ros1_ws/devel/setup.bash ]; then
+  source /root/ros1_ws/devel/setup.bash
+else
+  catkin_make
+  source devel/setup.bash
+fi
+
+exec "$@"
