4cee7899bfe9c60529ad5d264a5ba77ba7d9c206
Akshay Radhamohan Menon <akshay@senseglove.com>
2025-08-04 17:12:24 +0200

    things get launched but need to finalize hardware_interface

--- senseglove/senseglove_control/senseglove_hardware_interface/launch/hardware.launch.py
+++ senseglove/senseglove_control/senseglove_hardware_interface/launch/hardware.launch.py
@@ -1,17 +1,12 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, TextSubstitution
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution, TextSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PythonExpression
 
-
-import os
-from ament_index_python.packages import get_package_share_directory
-from xacro import process_file
-
 def generate_launch_description():
-    # Launch args
+
     robot = LaunchConfiguration('robot')
     glove_index = LaunchConfiguration('gloveIndex')
     is_right = LaunchConfiguration('isRight')
@@ -21,60 +16,70 @@ def generate_launch_description():
 
     xacro_file = PathJoinSubstitution([
         FindPackageShare('senseglove_description'),
-        'urdf', robot_type, TextSubstitution(text='/'), robot, '.xacro'
+        'urdf', 
+        robot_type, 
+        [robot, TextSubstitution(text='.urdf.xacro')]
     ])
 
-    robot_description = {
-        'robot_description': Command(['xacro', xacro_file])
-    }
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name='xacro')]),
+        " ",
+        xacro_file,
+        ])
+
+
+    robot_description = {'robot_description': robot_description_content}
 
     namespace = PathJoinSubstitution([
-        '/senseglove/glove',
-        PythonExpression(['str(', glove_index, ')']),
+        '/senseglove/',
+        PythonExpression(["'glove' + str(", glove_index, ")"]),
         handedness
     ])
 
-    controller_yaml_file = PathJoinSubstitution([
+    robot_controllers = PathJoinSubstitution([
         FindPackageShare('senseglove_hardware_interface'),
         'config',
         robot,
         'controllers.yaml'
     ])
 
-    ros2_control_node = Node(
+    control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
-        namespace=namespace,
-        parameters=[robot_description, controller_yaml_file],
-        output='screen'
+        parameters=[robot_controllers],
+        output='screen',
+        namespace=namespace
     )
 
     controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        namespace=namespace,
-        arguments=['joint_state_broadcaster', '--controller-manager-timeout', '60']
+        arguments=['joint_state_broadcaster', '--controller-manager-timeout', '60'],
+        output='screen',
+        namespace=namespace
     )
 
     trajectory_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        namespace=namespace,
-        arguments=['trajectory_controller', '--controller-manager-timeout', '60']
+        arguments=['trajectory_controller', '--controller-manager-timeout', '60'],
+        output='screen',
+        namespace=namespace
     )
 
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        namespace=namespace,
-        parameters=[robot_description, {'publish_frequency': 60}]
+        parameters=[robot_description],
+        output='screen',
+        namespace=namespace
     )
 
     return LaunchDescription([
         DeclareLaunchArgument('robot', description='The robot model to use'),
         DeclareLaunchArgument('gloveIndex', description='Index of the glove'),
         DeclareLaunchArgument('isRight', description='Is right hand glove? (true/false)'),
-        ros2_control_node,
+        control_node,
         controller_spawner,
         trajectory_controller_spawner,
         robot_state_publisher
--- senseglove/senseglove_launch/launch/senseglove.launch.py
+++ senseglove/senseglove_launch/launch/senseglove.launch.py
@@ -1,69 +1,75 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, LogInfo
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, LogInfo, OpaqueFunction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch.event_handlers import OnProcessStart
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
 import yaml
 import os
 
 def generate_launch_description():
     use_rviz = LaunchConfiguration('use_rviz', default='false')
 
-    # Locate this package's share dir
-    package_share = os.path.join(
-        os.getenv('AMENT_PREFIX_PATH').split(':')[0],
-        'share', 'senseglove_launch'
+    # Locate senseglove_com package
+    sensecom_share = get_package_share_directory('senseglove_com')
+    sensecom_bin = os.path.join(
+        sensecom_share, 'Linux', 'SenseCom_Linux_Latest', 'SenseCom.x86_64'
     )
-    config_dir = os.path.join(package_share, 'config')
 
     # Start SenseCom
     sensecom_process = ExecuteProcess(
-        cmd=[
-            os.path.join(
-                os.getenv('AMENT_PREFIX_PATH').split(':')[0],
-                'share', 'senseglove_com', 'SenseCom', 'Linux', 'SenseCom_Linux_Latest', 'SenseCom.x86_64'
-            )
-        ],
-        output='screen'
+        cmd=[sensecom_bin],
+        output='log'
     )
 
-    # Wait for user confirmation
-    wait_for_user = ExecuteProcess(
-        cmd=['python3', '-c', "input('Please confirm all gloves are connected in SenseCom and press ENTER to continue...')"],
-        output='screen'
-    )
+    # Locate senseglove_launch package
+    launch_share = get_package_share_directory('senseglove_launch')    
+    gloves_file = os.path.join(launch_share, 'config', 'gloves.yaml')
 
-    # Parse gloves.yaml
-    gloves_file = os.path.join(config_dir, 'gloves.yaml')
+    # Locate senseglove_hardware_interface package
+    hw_share = get_package_share_directory('senseglove_hardware_interface')    
+    hardware_launch = os.path.join(hw_share, 'launch','hardware.launch.py')
+
+    # Load gloves configuration
     with open(gloves_file, 'r') as f:
         config = yaml.safe_load(f)
     gloves = config.get('gloves', [])
 
-    hardware_nodes = []
-    for glove in gloves:
-        robot = glove.get('type', 'nova2') + '_' + glove.get('side', 'left')
-        index = str(glove.get('index', 0))
-        is_right = 'true' if glove.get('side') == 'right' else 'false'
+    def launch_hardware_nodes(context, *args, **kwargs):
+        input(" Starting SenseCom. Please confirm all gloves are connected in SenseCom and press ENTER to continue...")
+        
+        hardware_nodes = []
+        for glove in gloves:
+            robot = glove.get('type', 'nova2') + '_' + glove.get('side', 'left')
+            index = str(glove.get('index', 0))
+            is_right = 'true' if glove.get('side') == 'right' else 'false'
 
-        hardware_nodes.append(
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(
-                        os.path.dirname(__file__),
-                        'hardware.launch.py'
-                    )
-                ),
-                launch_arguments={
-                    'robot': robot,
-                    'gloveIndex': index,
-                    'isRight': is_right
-                }.items()
+            hardware_nodes.append(
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(hardware_launch),
+                    launch_arguments={
+                        'robot': robot,
+                        'gloveIndex': index,
+                        'isRight': is_right
+                    }.items()
+                )
             )
+        
+        return hardware_nodes
+
+    # Hardware Nodes Event handler
+    launch_hardware_nodes_handler = RegisterEventHandler(
+        OnProcessStart(
+            target_action=sensecom_process,
+            on_start=[OpaqueFunction(function=launch_hardware_nodes)]
         )
+    )
 
     # Calibration nodes
-    calib_left_file = os.path.join(config_dir, 'calibration_left.yaml')
-    calib_right_file = os.path.join(config_dir, 'calibration_right.yaml')
+    calib_left_file = os.path.join(launch_share, 'config', 'calibration_left.yaml')
+    calib_right_file = os.path.join(launch_share, 'config', 'calibration_right.yaml')
 
     # TODO: This needs a dynamic arg call
     calibration_left = Node(
@@ -98,13 +104,9 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('use_rviz', default_value='false'),
-
         sensecom_process,
-        wait_for_user,
-        LogInfo(msg='Launching gloves...'),
-
-        *hardware_nodes,
-        calibration_left,
-        calibration_right,
-        rviz_node
+        launch_hardware_nodes_handler 
+        # calibration_left,
+        # calibration_right,
+        # rviz_node
     ])
